80522aae6b4d7407bbe2fdfab7115ab27e2a84c5
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2019-09-14 13:53:00 +0300

    CDSCV-25 add RTSD and CR datasets

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -11,12 +11,14 @@ from .voc import VOCDataset
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
 from .coco_road import CocoRoadDataset
+from .rtsd import RTSDDataset, RTSDTenOneDataset
+from .crossroad import CrossroadDataset
 
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset',
     'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
     'build_dataloader', 'to_tensor', 'random_scale', 'show_ann',
     'ConcatDataset', 'RepeatDataset', 'ExtraAugmentation', 'WIDERFaceDataset',
-    'CocoRoadDataset',
+    'CocoRoadDataset', 'RTSDDataset', 'CrossroadDataset', 'RTSDTenOneDataset',
     'DATASETS', 'build_dataset'
 ]
